f477da4ff08db10e0145f0da1eb60f9b1f1476b1
jwang <jing.j.wang@intel.com>
2019-11-06 15:22:26 +0000

    Use TIME macro to org code

--- eigen_blas_perf.cpp
+++ eigen_blas_perf.cpp
@@ -10,18 +10,22 @@ using namespace Eigen;
 #define N 2000
 #define K 1024
 
+#define TIME(func, desc) {\
+		printf("%s\n", desc); \
+		clock_t t1 = clock(); \
+		func;\
+		clock_t t2 = clock(); \
+		printf("\ttime taken: %.2f second.\n", \
+					((float)t2 - (float)t1) / CLOCKS_PER_SEC); \
+}
 void int_perf(){
 
     // multiplication by eigen
     MatrixXi a = (MatrixXd::Random(M, K) * 1024).cast<int>();
     MatrixXi b = (MatrixXd::Random(K, N) * 1024).cast<int>();
     MatrixXi d(M, N);
-
-    clock_t begin = clock();
-    d = a*b;
-    clock_t end = clock();
-    double sec = double(end - begin) / CLOCKS_PER_SEC;
-    std::cout << "integer time(eigen): " << sec << "\t[" << d(0, 0) << " ...]" << std::endl;
+    TIME((d=a*b), "eigen integer")
+    std::cout << "\t[" << d(0, 0) << " ...]" << std::endl;
 
     // multiplication by openblas
     const float alpha=1;
@@ -41,14 +45,11 @@ void int_perf(){
            b_arr[i * N + j] = b(i, j);
        }
     }
-
     memset(c_arr, 0, M*N*sizeof(float));
 
-    begin = clock();
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha, a_arr, K, b_arr, N, beta, c_arr, N);
-    end = clock();
-    sec = double(end - begin) / CLOCKS_PER_SEC;
-    std::cout << "integer time(blas ): " << sec << "\t[" << int(c_arr[0]) << " ...]" << std::endl;
+    TIME(cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha, a_arr, K, b_arr, N, beta, c_arr, N), 
+		    "blas integer")
+    std::cout <<"\t[" << c_arr[0] << " ...]" << std::endl;
 
     free(a_arr);
     free(b_arr);
@@ -60,12 +61,10 @@ void double_perf(){
     MatrixXd b = MatrixXd::Random(K, N);
     MatrixXd d(M, N);
 
-    clock_t begin = clock();
-    d = a*b;
-    clock_t end = clock();
-    double sec = double(end - begin) / CLOCKS_PER_SEC;
-    std::cout << "double  time(eigne): " << sec << "\t[" << d(0, 0)<< " ...]" << std::endl;
+    TIME((d = a * b), "eigen double")
+    std::cout << "\t[" << d(0, 0) << " ...]" << std::endl;
 
+    // multiplication by openblas
     const float alpha=1;
     const float beta=0;
     double *a_arr = (double *)memalign(64, M*K*sizeof(double));
@@ -84,11 +83,9 @@ void double_perf(){
        }
     }
 
-    begin = clock();
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha, a_arr, K, b_arr, N, beta, c_arr, N);
-    end = clock();
-    sec = double(end- begin) / CLOCKS_PER_SEC;
-    std::cout << "double  time(blas ): " << sec << "\t[" << c_arr[0] << " ...]" << std::endl;
+    TIME(cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, alpha, a_arr, K, b_arr, N, beta, c_arr, N),
+		    "blas double")
+    std::cout <<"\t[" << c_arr[0] << " ...]" << std::endl;
 
     free(a_arr);
     free(b_arr);
